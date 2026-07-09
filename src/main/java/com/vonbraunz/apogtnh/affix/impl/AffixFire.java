281a339c9b2187db01e13ea4a503693878928fbd
VonBraunZ <vonbraunz@gmail.com>
2026-07-08 22:19:01 -0600

    fix fire affix tooltip formatting with Roman numerals

--- src/main/java/com/vonbraunz/apogtnh/affix/impl/AffixFire.java
+++ src/main/java/com/vonbraunz/apogtnh/affix/impl/AffixFire.java
@@ -24,7 +24,7 @@ public class AffixFire extends Affix {
 
     @Override
     public String tooltip(ItemStack stack, int level) {
-        return "+" + toRoman(level) + "s Fire";
+        return "+" + toRoman(level) + " Fire";
     }
 
     @Override
