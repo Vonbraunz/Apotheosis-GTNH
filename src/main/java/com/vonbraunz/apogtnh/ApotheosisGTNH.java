d77cf48ce581c50ee5eaf9b1b8bc6531ed3fa44d
VonBraunZ <vonbraunz@gmail.com>
2026-08-04 00:16:05 -0600

    bump version to 2.0.0

--- src/main/java/com/vonbraunz/apogtnh/ApotheosisGTNH.java
+++ src/main/java/com/vonbraunz/apogtnh/ApotheosisGTNH.java
@@ -25,7 +25,7 @@ public class ApotheosisGTNH {
 
     public static final String MODID = "apogtnh";
     public static final String NAME = "ApotheosisGTNH";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "2.0.0";
 
     @Instance(MODID)
     public static ApotheosisGTNH instance;
